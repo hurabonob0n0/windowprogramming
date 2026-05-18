957b5c0b176156b3c7389e7208559413ff539f8d
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2026-05-18 19:24:56 +0900

    최신화

--- FianlProject/Private/BaseSpriteObject.cpp
+++ FianlProject/Private/BaseSpriteObject.cpp
@@ -7,14 +7,14 @@ GameObject* BaseSpriteObject::Initialize()
 	__super::Initialize();
 
 	m_Sprite = Add_Component<Sprite>();
-	m_Sprite->Load_Image(L"../Sprites/miho2.png");
-	
+	//m_Sprite->Load_Image(L"../Sprites/miho2.png");
+	m_Sprite->Load_Image(L"../Sprites/jammanbo_12_4_3.png");
 
 	m_VB = Add_Component<VertexBuffer>();
 	m_VB->Set_Shape(QUAD);
 
 
-	m_Transform->Set_TransformState(0, 0, 500, 500, 0);
+	m_Transform->Set_TransformState(0, 0, 200, 200, 0);
 	//m_Transform->set
 	return this;
 }
@@ -29,6 +29,8 @@ void BaseSpriteObject::Update(float dt)
 		m_Transform->Go_Down(1000 * dt);
 	if (g_RawInput->Key_Pressing('D'))
 		m_Transform->Go_Right(1000 * dt);
+
+	m_Sprite->Update(dt);
 }
 
 void BaseSpriteObject::Late_Update(float dt)
